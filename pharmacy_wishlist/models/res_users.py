328181b7c9124ac1805597cbacc7234a01f01b72
StevenBahaa <stevenbahaa08@gmail.com>
2026-05-07 22:34:27 +0300

    Move wishlist security to pharmacy_base and switch to Cashier group

--- pharmacy_wishlist/models/res_users.py
+++ pharmacy_wishlist/models/res_users.py
@@ -6,9 +6,15 @@ class ResUsers(models.Model):
 
     wishlist_permission = fields.Boolean(
         string='Can Manage POS Wishlist',
-        default=False,
-        help='If checked, the user will see the Wishlist button in the POS.'
+        compute='_compute_wishlist_permission',
+        help='Automatically enabled for Cashiers and Pharmacy Managers.'
     )
+
+    def _compute_wishlist_permission(self):
+        group_cashier = 'pharmacy_base.group_cashier'
+        group_manager = 'pharmacy_base.group_pharmacy_manager'
+        for user in self:
+            user.wishlist_permission = user.has_group(group_cashier) or user.has_group(group_manager)
 
     def _load_pos_data_fields(self, config_id):
         fields_list = super()._load_pos_data_fields(config_id)
